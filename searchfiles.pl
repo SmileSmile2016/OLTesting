e5f184c9b2694a6b27ae8c191d53e79c905070fe
LeiYangGH <675686066@qq.com>
2016-10-20 03:25:55 -0700

    can search in linux

--- searchfiles.pl
+++ searchfiles.pl
@@ -5,44 +5,20 @@ use strict;
 use warnings;
 use File::Find;
 my $folder='/home/';
+my $find = 'leiyang|ly|yanglei2'; 
 find(sub{
-	local $fn = $_;
+	my $fn = $_;
 	if($fn =~ /^.{2,20}$/)
 	{
-		print "$fn\n";
+     		open(FILE, $fn); #|| die("can't open datafile: $!");
+     		while (<FILE>) 
+     		{
+         		if ($_ =~ /$find/)
+          		{
+             			print "$&\t";
+             			print "$fn\n\n";
+          		}
+     		}
 	}
-	
 	},$folder);
 
-
-# opendir(DIR, $folder);
-# @files = readdir(DIR);
-
-
-# print "====start=======\n\n";
-
-# foreach $fn (@files)
-# {
-# my $find = 'leiyang|ly|yanglei2';
-
-# if($fn =~ /^.{2,20}$/)
-# {
-    # my $fullname = "${folder}/${fn}";
-    # #print "$fullname\n\n";
-
-    # open(FILE, $fullname); #|| die("can't open datafile: $!");
-    # while (<FILE>) 
-    # {
-        # if ($_ =~ /$find/)
-         # {
-            # print "$&\t";
-            # print "$fullname\n\n";
-         # }
-    # }
-    # #print "\n\n";
-# }
-
- # } 
-
-# closedir DIR;
-print "====end=======\n\n";
